77d3915a2693627a88e3706a80daa7ae2d207a90
patrick <patrick@enburg.info>
2019-11-23 18:34:27 -0800

    Initial Multiple Choice Rendering

--- eclipse-project/TriviaMazeBlue42CSCD350/src/dungeon/MultipleChoice.java
+++ eclipse-project/TriviaMazeBlue42CSCD350/src/dungeon/MultipleChoice.java
@@ -1,73 +1,114 @@
 package dungeon;
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
 public class MultipleChoice extends Question
 {
-	
-	private String btn1, btn2, btn3, btn4;
+	private String[] options;
+	private int optionSelected = 0;
 	private int correctBtn;
 	
 	public MultipleChoice(String btn1, String btn2, String btn3, String btn4, int correctAnswer, String question, String explanation)
 	{
+		this.options = new String[4];
 		this.question = question;
 		this.explanation = explanation;
-		this.btn1 = btn1;
-		this.btn2 = btn2;
-		this.btn3 = btn3;
-		this.btn4 = btn4;
-		this.correctBtn = correctAnswer;
+		this.options[0] = btn1;
+		this.options[1] = btn2;
+		this.options[2] = btn3;
+		this.options[3] = btn4;
+		this.correctBtn = correctAnswer - 1;
 	}
 	
-	public String getBtn1()
-	{
-		return this.btn1;
-	}
-	
-	public String getBtn2()
-	{
-		return this.btn2;
-	}
-	
-	public String getBtn3()
-	{
-		return this.btn3;
-	}
-	
-	public String getBtn4()
-	{
-		return this.btn4;
-	}
-	
-	public int getCorrectAnswer()
-	{
-		return this.correctBtn;
-	}
-	
-	public String getQuestion()
-	{
-		return this.question;
-	}
-	
-	public String getExplanation()
-	{
-		return this.explanation;
-	}
+//	public String getBtn1()
+//	{
+//		return this.btn1;
+//	}
+//	
+//	public String getBtn2()
+//	{
+//		return this.btn2;
+//	}
+//	
+//	public String getBtn3()
+//	{
+//		return this.btn3;
+//	}
+//	
+//	public String getBtn4()
+//	{
+//		return this.btn4;
+//	}
+//	
+//	public int getCorrectAnswer()
+//	{
+//		return this.correctBtn;
+//	}
+//	
+//	public String getQuestion()
+//	{
+//		return this.question;
+//	}
+//	
+//	public String getExplanation()
+//	{
+//		return this.explanation;
+//	}
 
 	@Override
 	public void onUp() {
-		// TODO Auto-generated method stub
-		
+		optionSelected = (optionSelected - 1 + options.length) % options.length;
 	}
 
 	@Override
 	public void onDown() {
-		// TODO Auto-generated method stub
-		
+		optionSelected = (optionSelected + 1 + options.length) % options.length;
 	}
 
 	@Override
 	public void onEnter() {
-		// TODO Auto-generated method stub
+		this.QuestionCorrect = (optionSelected == correctBtn);
+		this.QuestionSubmitted.set(true);
+	}
+	
+	@Override
+	public void draw(double imgX, double imgY, int x, int y, double offset, Canvas canvas) {
+		super.draw(imgX, imgY, x, y, offset, canvas);
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		gc.save();
+		gc.setStroke(Color.BLACK);
+        gc.setFill(Color.WHITE);
+        gc.setLineWidth(lineWidth);
+        gc.setFont(new Font(fontString, fontSize * offset));
+        
+		double tenPercent = offset * 0.1f;
+        double fivePercent = offset * 0.05f;
+        double thirtyFivePercent = offset * 0.35f;
+        
+		double listSpace = fivePercent;
+		double listX = imgX + tenPercent;
+        double listY = imgY + thirtyFivePercent + thirtyFivePercent + tenPercent;
 		
+        double messageTopX = imgX;
+        double messageTopY = imgY + tenPercent + tenPercent;
+        
+        gc.fillText(this.question, messageTopX, messageTopY);
+        gc.strokeText(this.question, messageTopX, messageTopY);
+        
+        
+        for (int i = 0; i < options.length; i++) {
+            gc.fillText(options[i], listX, listY + (i * listSpace));
+            gc.strokeText(options[i], listX, listY + (i * listSpace));
+            if (i == optionSelected) {
+                gc.fillText("->", listX - fivePercent, listY + (i * listSpace));
+                gc.strokeText("->", listX - fivePercent, listY + (i * listSpace));
+            }
+        }
+        
+        gc.restore();
 	}
 	
 }
