71751a5e11fa3c4838027c338011e64120db9bc1
patrick <patrick@enburg.info>
2019-11-23 18:27:31 -0800

    Initial T/F question rendering

--- eclipse-project/TriviaMazeBlue42CSCD350/src/dungeon/TrueFalse.java
+++ eclipse-project/TriviaMazeBlue42CSCD350/src/dungeon/TrueFalse.java
@@ -1,61 +1,102 @@
 package dungeon;
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
 public class TrueFalse extends Question
 {
 
-	private String btn1, btn2;
+	private final String[] options = {"True", "False"};
+	private int optionSelected = 0;
+	
 	private int correctBtn;
 	
 	public TrueFalse(int correctAnswer, String question, String explanation)
 	{
 		this.question = question;
 		this.explanation = explanation;
-		this.btn1 = "True";
-		this.btn2 = "False";
-		this.correctBtn = correctAnswer;
+		this.correctBtn = correctAnswer - 1;
 	}
 
-	public String getBtn1()
-	{
-		return this.btn1;
-	}
-	
-	public String getBtn2()
-	{
-		return this.btn2;
-	}
-	
-	public int getCorrectAnswer()
-	{
-		return this.correctBtn;
-	}
-	
-	public String getQuestion()
-	{
-		return this.question;
-	}
-	
-	public String getExplanation()
-	{
-		return this.explanation;
-	}
+//	public String getBtn1()
+//	{
+//		return this.btn1;
+//	}
+//	
+//	public String getBtn2()
+//	{
+//		return this.btn2;
+//	}
+//	
+//	public int getCorrectAnswer()
+//	{
+//		return this.correctBtn;
+//	}
+//	
+//	public String getQuestion()
+//	{
+//		return this.question;
+//	}
+//	
+//	public String getExplanation()
+//	{
+//		return this.explanation;
+//	}
 
 	@Override
 	public void onUp() {
-		// TODO Auto-generated method stub
-		
+		optionSelected = (optionSelected - 1 + options.length) % options.length;
 	}
 
 	@Override
 	public void onDown() {
-		// TODO Auto-generated method stub
-		
+		optionSelected = (optionSelected + 1 + options.length) % options.length;
 	}
 
 	@Override
 	public void onEnter() {
+		this.QuestionCorrect = (optionSelected == correctBtn);
+		this.QuestionSubmitted.set(true);
+	}
+	
+	@Override
+	public void draw(double imgX, double imgY, int x, int y, double offset, Canvas canvas) {
 		// TODO Auto-generated method stub
+		super.draw(imgX, imgY, x, y, offset, canvas);
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		gc.save();
+		gc.setStroke(Color.BLACK);
+        gc.setFill(Color.WHITE);
+        gc.setLineWidth(lineWidth);
+        gc.setFont(new Font(fontString, fontSize * offset));
+        
+		double tenPercent = offset * 0.1f;
+        double fivePercent = offset * 0.05f;
+        double thirtyFivePercent = offset * 0.35f;
+        
+		double listSpace = fivePercent;
+		double listX = imgX + tenPercent;
+        double listY = imgY + thirtyFivePercent + thirtyFivePercent + tenPercent;
 		
+        double messageTopX = imgX;
+        double messageTopY = imgY + tenPercent + tenPercent;
+        
+        gc.fillText(this.question, messageTopX, messageTopY);
+        gc.strokeText(this.question, messageTopX, messageTopY);
+        
+        
+        for (int i = 0; i < options.length; i++) {
+            gc.fillText(options[i], listX, listY + (i * listSpace));
+            gc.strokeText(options[i], listX, listY + (i * listSpace));
+            if (i == optionSelected) {
+                gc.fillText("->", listX - fivePercent, listY + (i * listSpace));
+                gc.strokeText("->", listX - fivePercent, listY + (i * listSpace));
+            }
+        }
+        
+        gc.restore();
 	}
 
 }
