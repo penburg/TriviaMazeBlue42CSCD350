d4e401cb40c5f7507ef8a52cd880f0a1b5249e09
Terrance <terrancejcunningham@gmail.com>
2019-11-24 17:47:59 -0800

    Added Traverse method in Dungeon. KEY  = "T" to test out

--- eclipse-project/TriviaMazeBlue42CSCD350/src/dungeon/DungeonAdventure.java
+++ eclipse-project/TriviaMazeBlue42CSCD350/src/dungeon/DungeonAdventure.java
@@ -208,6 +208,9 @@ public class DungeonAdventure extends Application {
             case U:
             	mGame.onDoorCheatCode();
             	break;
+            case T:
+            	mGame.traverseMaze();
+            	break;
             case ENTER:
                 mGame.onEnter();
                 break;
